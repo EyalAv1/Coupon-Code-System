c8c0e7c9a9f4c36e9b9c5fa3a350a6d8d2163bcd
EyalAv1 <eyalavni6@gmail.com>
2024-11-06 11:56:37 +0200

    fixig the coupons cards

--- src/components/adminFunctionsItem/FunctionsItem.tsx
+++ src/components/adminFunctionsItem/FunctionsItem.tsx
@@ -1,3 +1,17 @@
-export default function FunctionItem() {
-  return <div></div>;
+import "./FunctionsItem.css";
+
+interface ItemProps{
+    couponName : string;
+    couponCode : string;
+    couponDiscount: number;
+}
+
+export default function CouponCardItem({couponName, couponCode, couponDiscount} : ItemProps) {
+  return (
+    <div>
+      <div>{couponName}</div>
+      <div>{couponCode}</div>
+      <div>{couponDiscount}</div>
+    </div>
+  );
 }
---
+++ src/components/userCouponsValidator/CouponsValidator.tsx
@@ -0,0 +1,12 @@
+import "./CouponsValidator.css";
+
+export default function CouponsValidatoer() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Enter your Coupon" />
+        <input type="button" value="Check Coupon" />
+      </form>
+    </div>
+  );
+}

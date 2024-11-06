b69d16c9e6364363e963ae6ce8878c5d1ad56a42
EyalAv1 <eyalavni6@gmail.com>
2024-11-06 15:02:00 +0200

    Init login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./containers/mainPage/MainPage";
 import AdminCoupons from "./containers/adminCouponsPage/AdminCoupons";
 import NavigationBar from "./components/navigation/NavigationBar";
+import Login from "./containers/login/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/adminCoupons" element={<AdminCoupons />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/adminFunctionsItem/FunctionsItem.tsx
@@ -0,0 +1,21 @@
+import "./FunctionsItem.css";
+
+interface ItemProps {
+  couponName: string;
+  couponCode: string;
+  couponDiscount: number;
+}
+
+export default function CouponCardItem({
+  couponName,
+  couponCode,
+  couponDiscount,
+}: ItemProps) {
+  return (
+    <div>
+      <div>{couponName}</div>
+      <div>{couponCode}</div>
+      <div>{couponDiscount}</div>
+    </div>
+  );
+}

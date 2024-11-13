de6427f91df4d60997d9ed3d8e296bf90f8c5017
EyalAv1 <eyalavni6@gmail.com>
2024-11-13 18:56:56 +0200

    test for deploy bug

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { UserProvider } from "./Context/userContext.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <UserProvider>
         <App />
       </UserProvider>
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
